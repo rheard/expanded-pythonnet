0f760150e7f7abcbb4bf922bf2e4f6658b031a47
Ryan Heard <ryanwheard@gmail.com>
2020-08-02 04:26:22 -0500

    second commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+__author__ = ()
+__version__ = "0.0.1"
+
+from setuptools import setup
+
+
+setup(name='expanded-pythonnet',
+      version=__version__,
+      description="A more pythonic wrapper around PythonNet",
+      url='TBD',
+      packages=['expanded_clr'],
+      install_requires=['pythonnet'])
