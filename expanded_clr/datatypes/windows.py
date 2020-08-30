9509910696211aa6edaa09439cd628a40e52f927
Ryan Heard <ryanwheard@gmail.com>
2020-08-29 19:04:45 -0500

    Add access points for System.Windows.Rect, Vector and Point

--- expanded_clr/datatypes/windows.py
+++ expanded_clr/datatypes/windows.py
@@ -1,3 +1,8 @@
-from ..utils import csharp_namedtuple
+from clr import System
+
+from ..utils import csharp_namedtuple, get_wrapper_class
 
 Padding = csharp_namedtuple('Padding', 'Left Top Right Bottom')
+Rect = get_wrapper_class(System.Windows.Rect)
+Vector = get_wrapper_class(System.Windows.Vector)
+Point = get_wrapper_class(System.Windows.Point)
