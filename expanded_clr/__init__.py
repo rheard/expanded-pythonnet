7279f665942773c53689c5a250fe899e1bcddaa5
Ryan Heard <ryanwheard@gmail.com>
2020-09-19 20:09:04 -0500

    Add WindowsBase reference

--- expanded_clr/__init__.py
+++ expanded_clr/__init__.py
@@ -1,2 +1,8 @@
+import clr
+
+# TODO: Build a searching mechanism (is there one in pythonnet or C#) so these don't need to be in the cwd.
+clr.AddReference('WindowsBase')
+
+
 from . import utils, converters, datatypes
 from .utils import get_wrapper_class, wrap_python_method, wrap_csharp_method
